504575c870dfecfe1cb0d38172bbdd8842e95c07
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-05-03 12:10:49 +0200

    homework from modul 4

--- venv/modul-5-1.py
+++ venv/modul-5-1.py
@@ -1,1 +1,15 @@
-name = 'kuba'
+def group_them(**kwargs):
+    result = ''
+    for key, value in kwargs.items():
+        # print(f'{key.capitalize()} is {value}')
+        result += key.capitalize() + ' is ' + value + '\n'
+    return result
+
+
+def test_group_them(capsys):
+    group_them(wall='red', tomato='red', light='yellow')
+    out, err = capsys.readouterr()
+    assert out == 'Wall is red\nTomato is red\nLight is yellow\n'
+
+
+print(group_them(wall='red', tomato='red', light='yellow'))
