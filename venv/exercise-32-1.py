700668ac0a95d3ff4ceb3a09304dd7fe0b204ab8
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-05-07 14:16:04 +0200

    I added up the values transferred to a separate file from the previous function.

--- venv/exercise-32-1.py
+++ venv/exercise-32-1.py
@@ -19,3 +19,16 @@ with open('income.txt', 'a') as output_file:
         output_file.write(f'{transaction}\n')
 
 
+result = 0
+
+with open('income.txt', 'r') as handler:
+    income_statement = handler.readlines()
+    # print(income_statement)
+    for line in income_statement:
+        values = line.strip().split(',')
+        # print(values)
+        date, transaction_name, income_value = values
+        print(income_value)
+        result += int(income_value)
+
+print(f'Podliczenie dochodow. Zarobiłem: {result} erło..')
